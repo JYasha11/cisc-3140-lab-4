c35b123eb65e59017fb8022fc46c2b552e15c8ab
Edward Lee <93949554+Ed-ward239@users.noreply.github.com>
2022-12-06 19:07:35 -0500

    added something

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
+import DataLists from './Components/DataLists';
 
 function App() {
   return (
-    <div className="list">
-      <ul>
-        <li>Squirrel sighting ID</li>
-        <li>Color Key</li>
-        <li>Activity</li>
-        <li>Age</li>
-        <li>Date</li>
-        <li>Time</li>
-        <li>Location ID</li>
-        <li></li>
-        <li></li>
-      </ul>
-    </div>
+    <>
+      <div className="list">
+        <ul>
+          <li></li>
+          <li>Squirrel sighting ID</li>
+          <li>Color Key</li>
+          <li>Activity</li>
+          <li>Age</li>
+          <li>Date</li>
+          <li>Time</li>
+          <li>Location ID</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <DataLists />
+    </>
+
   );
 }
 
